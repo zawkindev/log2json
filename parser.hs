848866364024ca6beedd994b430afd5d166a0985
zawkindev <zawkindev@gmail.com>
2025-02-12 00:57:14 +0500

    feat: find index function

--- parser.hs
+++ parser.hs
@@ -1,6 +1,22 @@
 import System.IO
 
-main :: IO()
+extractInfo :: String -> String
+extractInfo str = key ++ ":" ++ value
+  where
+    (key, value) = span (/= '=') str
+
+findIndex :: Char -> String -> Int -> Int
+findIndex c "" n = n
+findIndex c str n =
+  if c == head str
+    then n
+    else findIndex c (drop 1 str) (n + 1)
+
+parse :: String -> String
+parse "" = ""
+parse (c : rest) = show c ++ '\n' : parse rest
+
+main :: IO ()
 main = do
-  contents <- readFile "test.log" 
-  print contents
+  contents <- readFile "file.log"
+  print (findIndex '=' contents 0)
